7f73c1a23d23d5341d6bf2e37254a756643372ef
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-09-18 09:48:53 -0700

    adding a html

--- workflow/scripts/make_trackdb.py
+++ workflow/scripts/make_trackdb.py
@@ -95,7 +95,7 @@ def html_table(color_dict, h1, h2, rgb=True):
     rtn += f"<tr><th>{h1}</th><th>{h2}</th></tr>"
     for key, value in color_dict.items():
         if rgb:
-            second = f'<div style="font-size:50px; color:rgb({value})">&#9623;</div>'
+            second = f'<div style="color:rgb({value})">&#9623;</div>'
         else:
             second = value
         rtn += '  <tr><td style="text-align: center; vertical-align: middle;">'
