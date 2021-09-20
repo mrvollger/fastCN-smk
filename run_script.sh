617b3c186335e5f995b70309ac97035e01f3fa1f
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-09-20 09:33:42 -0700

    adding load to aln so that it doesn't submit too many split jobs

--- run_script.sh
+++ run_script.sh
@@ -7,5 +7,11 @@ snakemake \
     --drmaa " -l centos=7 -l h_rt=48:00:00 -l mfree={resources.mem}G -pe serial {threads} -V -cwd -S /bin/bash -w n" \
     --drmaa-log-dir logs/drmaa \
     --use-conda \
+    --configfile config/config.yaml \
+    --local-cores 20 \
+    --cores 20 \
+    --max-inventory-time 10000 \
     --resources load=1000 \
+    --scheduler greedy \
+    --latency-wait 60 \
     "$@"
