c6cddf8915637665d9c65d038948f1464d6a6d16
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-11-18 09:33:02 -0800

    black formatting

--- workflow/scripts/compress_mrsfast.py
+++ workflow/scripts/compress_mrsfast.py
@@ -4,32 +4,30 @@
 
 import gzip
 
-file = gzip.open(snakemake.input.sam, 'rt')
+file = gzip.open(snakemake.input.sam, "rt")
 
 count = 0
 
 line_cols = []
 
 
-with gzip.open(snakemake.output.comp, 'wt') as outfile:
-    outfile.write('qname,flag,rname,pos,mapq\n')
+with gzip.open(snakemake.output.comp, "wt") as outfile:
+    outfile.write("qname,flag,rname,pos,mapq\n")
     while True:
         line = file.readline().rstrip()
         if not line:
             break
-        if line[0] == '@':
+        if line[0] == "@":
             continue
-        if line.split('\t')[1:] == line_cols:
+        if line.split("\t")[1:] == line_cols:
             count += 1
         elif line_cols == []:
-            line_cols = line.split('\t')[1:]
+            line_cols = line.split("\t")[1:]
             count = 1
         else:
-            out_string = ",".join([str(count)]+line_cols)
+            out_string = ",".join([str(count)] + line_cols)
             outfile.write(f"{out_string}\n")
-            line_cols = line.split('\t')[1:]
+            line_cols = line.split("\t")[1:]
             count = 1
 
 file.close()
-
-
--- workflow/scripts/unpack_partial_sam.py
+++ workflow/scripts/unpack_partial_sam.py
@@ -5,22 +5,20 @@
 
 import gzip
 
-file = gzip.open(snakemake.input.comp, 'rt')
+file = gzip.open(snakemake.input.comp, "rt")
 
 # Consume header
 line = file.readline()
 
-with open(snakemake.output.exp, 'w') as out_file:
-	out_file.write('@\n')
-	while True:
-		line = file.readline().rstrip()
-		if not line:
-			break
-		else:
-			line = line.split(',')
-			out_file.write('\n'.join(['\t'.join(line)] * int(line[0])))
-			out_file.write('\n')
+with open(snakemake.output.exp, "w") as out_file:
+    out_file.write("@\n")
+    while True:
+        line = file.readline().rstrip()
+        if not line:
+            break
+        else:
+            line = line.split(",")
+            out_file.write("\n".join(["\t".join(line)] * int(line[0])))
+            out_file.write("\n")
 
 file.close()
-
-
